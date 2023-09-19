9652cff87d904085b738a897f6e0497b4342793b
phat <phatdo436@gmail.com>
2023-09-19 16:17:18 +0700

    make slide

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,13 +1,60 @@
 import React from "react";
-import slider from "../../assets/image/slide-image.jpg";
 import "./slide.scss";
+import { Autoplay, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { slides } from "../../data";
+import { EffectCoverflow } from "swiper/modules";
 
 const Slide = () => {
+  const breakpoints = {
+    sx: 320,
+    sm: 425,
+    md: 650,
+    lg: 768,
+    xl: 1024,
+    xxl: 1440,
+  };
+
   return (
     <div className="container ">
-      <a href="/">
-        <img src={slider} alt="" className="slide__img" />
-      </a>
+      <Swiper
+        breakpoints={{
+          [breakpoints.xxl]: {
+            slidesPerView: 1,
+          },
+          [breakpoints.xl]: {
+            slidesPerView: 1,
+          },
+          [breakpoints.lg]: {
+            slidesPerView: 1,
+          },
+          [breakpoints.md]: {
+            slidesPerView: 1,
+          },
+          [breakpoints.sm]: {
+            slidesPerView: 1,
+          },
+          [breakpoints.sx]: {
+            slidesPerView: 1,
+          },
+        }}
+        autoplay={{
+          delay: 3000,
+        }}
+        slidesPerView={2}
+        modules={[Navigation, Autoplay, EffectCoverflow]}
+        effect="coverflow"
+      >
+        {slides.map((item, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <a className="wrapper__slide-img" href="/">
+                <img src={item.image} alt="" className="slide__img" />
+              </a>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 };
